2e2e1c1a33eb77e668eb013f0a8eac73a7da9f1e
Balakumar <balap021@gmail.com>
2014-08-31 21:36:21 +0530

    modified home page

---
+++ Bala/list.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css"  href="nav.css">
+<style>
+#header
+{
+position:absolute;
+height:160px;
+width:100%;
+border:1;
+background-color:black;
+background-image:url("")
+}
+
+#footer
+{
+position:absolute;
+bottom:-10;
+height:50;
+width:100%;
+background-color:white;
+}
+
+
+</style>
+</head>
+<body>
+<div id="header" align="left">
+<nav>
+
+<ul>
+<li><background-color:white>white<li>
+<li><background-color:red>red<li>
+</ul>
+</nav>
+<div id="footer">
+<table style="width:700px">
+<th><a href="home.html">home</a></th>
+<th><a href="home.html">user</a></th>
+<th><a href="home.html">System</a></th>
+<th><a href="home.html">student</a></th>
+</body>
+</html>
